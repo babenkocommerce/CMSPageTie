06991cf1e9549c7f608a7a57c843233decc9acbe
Alexey Rakitin <alexey.rakitin@babenkocommerce.com>
2019-10-23 13:27:10 +0300

    CPT-13: Drop-downs disable according to selected page store view

---
+++ view/adminhtml/web/js/form/element/ui-select-page.js
@@ -0,0 +1,60 @@
+define([
+    'Magento_Ui/js/form/element/ui-select',
+    'underscore'
+], function (UiSelect, _) {
+    return UiSelect.extend({
+        defaults: {
+            listens: {
+                '${ $.provider }:data.store_id': 'onPageStoreIdChange'
+            }
+        },
+
+        /**
+         * @inheritDoc
+         */
+        initialize: function() {
+            this._super();
+            this.setDisabled();
+        },
+
+        /**
+         * Listen to changes of selected StoreViews
+         * @param selectedStoreViews
+         */
+        onPageStoreIdChange: function (selectedStoreViews) {
+            this.setDisabled(selectedStoreViews);
+        },
+
+        /**
+         * Disable component if it's storeView is used by current CMS Page
+         * or if current CMS Page has 'All Store Views' selected
+         * @param selectedStoreViews
+         */
+        setDisabled: function (selectedStoreViews) {
+            var elementStoreView = this.source.get(this.parentScope + '.store_view'),
+                storeViews = selectedStoreViews ? selectedStoreViews : this.source.get('data.store_id');
+
+            if (
+                _.contains(storeViews, '0') ||
+                _.contains(storeViews, elementStoreView)
+            ) {
+                this.disable();
+            } else {
+                this.enable();
+            }
+        },
+
+        /**
+         * Toggle list visibility if component is enabled
+         *
+         * @returns {Object} Chainable
+         */
+        toggleListVisible: function () {
+            if (!this.disabled()) {
+                this.listVisible(!this.listVisible());
+            }
+
+            return this;
+        },
+    });
+});
