e42d239b6cd5538bc3324390be5b074a9e32e7dd
Alexey Rakitin <alexey.rakitin@babenkocommerce.com>
2019-11-05 17:29:24 +0200

    Implemented CPT-32: Unlink scenarios added

--- view/adminhtml/web/js/form/element/ui-select-page.js
+++ view/adminhtml/web/js/form/element/ui-select-page.js
@@ -7,6 +7,7 @@ define([
 ], function (UiSelect, _, confirm, alert, $t) {
     return UiSelect.extend({
         defaults: {
+            indexedOptions: false,
             listens: {
                 '${ $.provider }:data.store_id': 'onPageStoreIdChange'
             }
@@ -69,31 +70,43 @@ define([
                 this.listVisible(false);
                 return this;
             }
-            if (data.available_in && data.available_in.length > 1) {
-                if (_.intersection(data.available_in, this.source.get('data.store_id')).length) {
-                    alert({
-                        content: $t('The page you are trying to select also belongs ' +
-                            'to Store of current page and therefore cannot be selected.')
-                    });
-                } else {
-                    confirm({
-                        content: $t('Selected page also belongs to other Stores, ' +
-                            'select it for those Stores accordingly?'),
-                        actions: {
-                            confirm: this.onConfirm.bind(this, data)
-                        }
-                    });
-                }
+            if (this.value() !== '0' && this.getIndexedOptions()[this.value()].available_in.length > 1) {
+                confirm({
+                    content: $t('Currently linked page is also linked to other Stores. ' +
+                        'If you continue, it will be unlinked from those Stores accordingly. Are you sure?'),
+                    actions: {
+                        confirm: this.onConfirmUnlink.bind(
+                            this,
+                            this.getIndexedOptions()[this.value()].available_in,
+                            data
+                        )
+                    }
+                });
                 return this;
+            } else if (data.available_in && data.available_in.length > 1) {
+                this.maybeLink(data);
+                return this;
+            } else {
+                return this._super();
             }
-            return this._super();
+        },
+
+        /**
+         * Get options indexed by value
+         * @returns {Object}
+         */
+        getIndexedOptions: function() {
+            if (!this.indexedOptions) {
+                this.indexedOptions = _.indexBy(this.options(),'value');
+            }
+            return this.indexedOptions;
         },
 
         /**
          * Get Cms Page Tie rows data indexed by store_id
          * @returns {Object}
          */
-        getIndexedOptions: function () {
+        getIndexedRows: function () {
             if (!this.source.data.indexed_cms_page_tie_rows) {
                 this.source.set('data.indexed_cms_page_tie_rows',
                     _.indexBy(this.source.data.cms_page_tie_rows, 'store_id')
@@ -106,16 +119,59 @@ define([
          * Select page for all Stores it belongs to
          * @param data
          */
-        onConfirm: function (data) {
+        onConfirmLink: function (data) {
             var self = this;
             _.forEach(data.available_in, function (store_id) {
                 self.source.set(
                     'data.cms_page_tie_rows.' +
-                    self.getIndexedOptions()[store_id].record_id +
+                    self.getIndexedRows()[store_id].record_id +
                     '.linked_page_id',
                     data.value
                 )
             });
+        },
+
+        /**
+         * Unlink page, previously linked to many Stores
+         * @param unlinkData
+         * @param linkData
+         */
+        onConfirmUnlink: function (unlinkData, linkData) {
+            var self = this;
+            _.forEach(unlinkData, function (store_id) {
+                self.source.set(
+                    'data.cms_page_tie_rows.' +
+                    self.getIndexedRows()[store_id].record_id +
+                    '.linked_page_id',
+                    '0'
+                )
+            });
+            if (linkData.available_in && linkData.available_in.length > 1) {
+                this.maybeLink(linkData);
+            } else {
+                this.value(linkData.value);
+            }
+        },
+
+        /**
+         * Check if selected page can be linked and ask user in popup
+         * @param data
+         */
+        maybeLink: function (data) {
+            if (_.intersection(data.available_in, this.source.get('data.store_id')).length) {
+                alert({
+                    content: $t('The page you are trying to select also belongs ' +
+                        'to Store of current page and therefore cannot be selected.')
+                });
+            } else {
+                confirm({
+                    content: $t('The page you are trying to select also belongs to other Stores. ' +
+                        'Select it for those Stores accordingly?'),
+                    actions: {
+                        confirm: this.onConfirmLink.bind(this, data)
+                    }
+                });
+            }
         }
     });
 });
