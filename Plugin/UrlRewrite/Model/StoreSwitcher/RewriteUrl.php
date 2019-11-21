360012d674e9b32fed6cf06fe500df28949223a3
Mike Kots <mikhail.kots@babenkocommerce.com>
2019-11-21 14:33:05 +0200

    CR Changes CPT-21: Add Plugin/observer CMS router

--- Plugin/UrlRewrite/Model/StoreSwitcher/RewriteUrl.php
+++ Plugin/UrlRewrite/Model/StoreSwitcher/RewriteUrl.php
@@ -6,6 +6,8 @@ use Flexor\CMSPageTie\Api\TieManagementInterface;
 use Magento\Framework\UrlInterface as UrlBuilder;
 use Magento\Cms\Model\ResourceModel\Page\CollectionFactory as CmsPageCollection;
 use Magento\UrlRewrite\Model\StoreSwitcher\RewriteUrl as StoreRewriteUrl;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Cms\Model\ResourceModel\PageFactory as CmsPageModelFactory;
 
 /**
  * Class RewriteUrl
@@ -34,23 +36,39 @@ class RewriteUrl
     private $urlBuilder;
 
     /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @var CmsPageModelFactory
+     */
+    private $cmsPageModelFactory;
+
+    /**
      * RewriteUrl constructor.
      * @param RequestFactory $requestFactory
      * @param TieManagementInterface $tieManagement
      * @param CmsPageCollection $cmsPageCollection
      * @param UrlBuilder $urlBuilder
+     * @param ScopeConfigInterface $scopeConfig
+     * @param CmsPageModelFactory $cmsPageModelFactory
      */
     public function __construct(
         RequestFactory $requestFactory,
         TieManagementInterface $tieManagement,
         CmsPageCollection $cmsPageCollection,
-        UrlBuilder $urlBuilder
+        UrlBuilder $urlBuilder,
+        ScopeConfigInterface $scopeConfig,
+        CmsPageModelFactory $cmsPageModelFactory
     )
     {
         $this->requestFactory = $requestFactory;
         $this->tieManagement = $tieManagement;
         $this->cmsPageCollection = $cmsPageCollection;
         $this->urlBuilder = $urlBuilder;
+        $this->scopeConfig = $scopeConfig;
+        $this->cmsPageModelFactory = $cmsPageModelFactory;
     }
 
     /**
@@ -66,17 +84,25 @@ class RewriteUrl
         $currentStoreId = $oldRewrite->getData()['store_id'];
         $request = $this->requestFactory->create(['uri' => $targetUrl]);
         $urlPath = ltrim($request->getPathInfo(), '/');
-
+        $isStoreCodeEnabled = $this->scopeConfig->getValue(
+            'web/url/use_store', \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+        if ($isStoreCodeEnabled) {
+            $urlPath = substr($urlPath,strrpos($urlPath,"/")+1);
+        }
         $currentPageCollection = $this->cmsPageCollection->create()->addFieldToFilter('identifier', $urlPath);
         $currentPage = $currentPageCollection->getData();
         if (!empty($currentPage)) {
             $currentPageIdentifier = (int) array_column($currentPage, 'page_id')[0];
-            $linkedPageId = $this->tieManagement->getLinkedCmsKey($currentPageIdentifier, $currentStoreId);
-            if (isset($linkedPageId)) {
-                $result = $this->urlBuilder->getUrl(null, ['_direct' => $linkedPageId]);
+            $linkedPageName = $this->tieManagement->getLinkedCmsKey($currentPageIdentifier, $currentStoreId);
+            $linkedPageId = $this->tieManagement->getLinkedCmsIdByStoreId($currentPageIdentifier, $currentStoreId);
+            $attachedStores = $this->cmsPageModelFactory->create()->lookupStoreIds($linkedPageId);
+
+            if (isset($linkedPageId) && $linkedPageId != 0) {
+                $this->urlBuilder->setScope($attachedStores[0]);
+                $result = $this->urlBuilder->getUrl(null, ['_direct' => $linkedPageName]);
             }
         }
-
         return $result;
     }
 }
